09b93ace3cccd7105ce529b78b70e91950fdfeb2
Bomboclack <158288280+Bomboclak007@users.noreply.github.com>
2025-12-02 21:54:32 +0200

    5.1.php

--- 5.1.php
+++ 5.1.php
@@ -1,30 +1,51 @@
+<?php
+
+class Triangle {
+
+    private $cathetus1;
+    private $cathetus2; // втр ктр
+
+    public function __construct($cathetus1, $cathetus2) {     // инлц констр
+        $this->cathetus1 = $cathetus1; // инцлз 1кт
+        $this->cathetus2 = $cathetus2;
+    }
+
+    public function calculateArea() {
+        return 0.5 * $this->cathetus1 * $this->cathetus2; // площ
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // пров отпр форм
+
+    $cathetus1 = floatval($_POST['cathetus1']); // преоб в веществ числ
+    $cathetus2 = floatval($_POST['cathetus2']);
+
+    $triangle = new Triangle($cathetus1, $cathetus2);
+
+    $area = $triangle->calculateArea();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Вычисление площади прямоугольного треугольника</title>
 </head>
 <body>
-<h1>Вычисление площади прямоугольного треугольника</h1>
-
-
+<h1>Введите катеты прямоугольного треугольника</h1>
 <form method="post">
-    <label for="catet1">Длина первого катета:</label><br>
-    <input type="number" step="0.01" name="catet1" id="catet1" required><br><br>
-    <label for="catet2">Длина второго катета:</label><br>
-    <input type="number" step="0.01" name="catet2" id="catet2" required><br><br>
+    <label for="cathetus1">Первый катет:</label>
+    <input type="text" id="cathetus1" name="cathetus1" required>
+    <br>
+    <label for="cathetus2">Второй катет:</label>
+    <input type="text" id="cathetus2" name="cathetus2" required>
+    <br>
     <input type="submit" value="Вычислить площадь">
 </form>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") { //отпр форм
-    $catet1 = (float)$_POST['catet1']; // перемен с плав точкой
-    $catet2 = (float)$_POST['catet2'];
-
-    $area = ($catet1 * $catet2) / 2;
-    echo "<h2>Площадь прямоугольного треугольника: " . $area . "</h2>";
-}
-?>
+<?php if (isset($area)): ?>
+    <h2>Площадь прямоугольного треугольника: <?php echo $area; ?></h2>
+<?php endif; ?>
 </body>
 </html>
