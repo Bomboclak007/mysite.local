cd6d8b7e0c4561c9c36e485a22afe696839b3c2a
Bomboclack <158288280+Bomboclak007@users.noreply.github.com>
2025-12-02 21:56:09 +0200

    5.1.php

--- 5.1.php
+++ 5.1.php
@@ -1,31 +1,22 @@
 <?php
-
 class Triangle {
-
     private $cathetus1;
     private $cathetus2; // втр ктр
-
     public function __construct($cathetus1, $cathetus2) {     // инлц констр
         $this->cathetus1 = $cathetus1; // инцлз 1кт
         $this->cathetus2 = $cathetus2;
     }
-
     public function calculateArea() {
         return 0.5 * $this->cathetus1 * $this->cathetus2; // площ
     }
 }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") { // пров отпр форм
-
     $cathetus1 = floatval($_POST['cathetus1']); // преоб в веществ числ
     $cathetus2 = floatval($_POST['cathetus2']);
-
     $triangle = new Triangle($cathetus1, $cathetus2);
-
     $area = $triangle->calculateArea();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -43,7 +34,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // пров отпр форм
     <br>
     <input type="submit" value="Вычислить площадь">
 </form>
-
 <?php if (isset($area)): ?>
     <h2>Площадь прямоугольного треугольника: <?php echo $area; ?></h2>
 <?php endif; ?>
